e900ab00f0e325d78dc13db1e7f76d06428001e8
Ford <weston0713@gmail.com>
2022-03-02 23:08:07 +0900

    check

---
+++ 07_command_pattern/01_install_wizard.py
@@ -0,0 +1,18 @@
+class Wizard:
+    
+    def __init__(self, src, rootdir):
+        self.choices = []
+        self.rootdir = rootdir
+        self.src = src
+        
+    def preferences(self, command):
+        self.choices.append(command)
+        
+    def execute(self):
+        for choice in self.choices:
+            if list(choice.values())[0]:
+                print("Copying binaries --", self.src, " to ", self.rootdir)
+            else:
+                print("No Operation")
+                
+# if __name__ == '__main__':
