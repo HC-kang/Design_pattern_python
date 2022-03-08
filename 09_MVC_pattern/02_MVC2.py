1d46ba5f38a11869345f21d81a3436ad883f4c9b
Ford <weston0713@gmail.com>
2022-03-08 16:13:56 +0900

    mvc

---
+++ 09_MVC_pattern/02_MVC2.py
@@ -0,0 +1,25 @@
+class Model:
+    def logic(self):
+        data = 'Got it!'
+        print("Model: Crunching data as per business logic")
+        return data
+    
+class View:
+    def update(self, data):
+        print("View: Updating the view with results: ", data)
+        
+class Controller:
+    def __init__(self):
+        self.model = Model()
+        self.view = View()
+        
+    def interface(self):
+        print("Controller: Relayed the Client asks")
+        data = self.model.logic()
+        self.view.update(data)
+        
+class Client:
+    print("Client: asks for certain information")
+    controller = Controller()
+    controller.interface()
+    
